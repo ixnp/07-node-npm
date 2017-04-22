49afee03ff9cce05578394e2566ba884e50e2c15
ixnp <enprocopios@gmail.com>
2017-04-21 19:23:56 -0700

    server not launching todos filled out

--- starter-code/server.js
+++ starter-code/server.js
@@ -4,17 +4,27 @@
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static(_dirname + '/public'));
 
-app.get('*', function(request, response) {
+app.get('/blog', function(request, response) {
   // TODO: Using the response object, send the index.html file back to the user
+  res.sendFile(_dirname + '/public/index.html')
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// app.get('/blog', function(request, response) {
+//   res.sendFile(_dirname + '/public/new.html')
+// });
+//idk if this will work//
 
 app.listen(PORT, function() {
+    console.log('server is running on port:' + PORT);
   // TODO: Log to the console a message that lets you know which port your server has started on
 });
